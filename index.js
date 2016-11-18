f00316b9ca5ea2ba740276b1d59a605869569f35
Maximilian Klein <mklein@welfenlab.de>
2016-11-18 23:47:04 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,92 @@
+
+const fs = require('fs')
+const getStdin = require('get-stdin')
+const spawn = require('child_process').spawn
+const tempfile = require('tempfile')
+const prompt = require('prompt-promise')
+
+function getFileContents (file) {
+  return new Promise((resolve, reject) => {
+    fs.exists(file, (err, exists) => {
+      if (err) {
+        reject(err)
+      } else {
+        fs.readFile(file, 'utf8', (err, contents) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(contents)
+          }
+        })
+      }
+    })
+  })
+}
+
+const edit = (file) => {
+  return new Promise((resolve, reject) => {
+    var editorCmd = process.env.EDITOR || 'nano'
+    var editor = spawn(editorCmd, [file], {stdio: 'inherit'})
+    editor.on('exit', () => {
+      fs.readFile(file, 'utf8', (err, contents) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(contents)
+        }
+      })
+    })
+  })
+}
+
+const editContent = (initialContent, fileType, verify) => {
+  var tmpFile = tempfile(fileType)
+  fs.writeFileSync(tmpFile, initialContent, 'utf8')
+  return edit(tmpFile)
+  .then((content) => {
+    if (!verify || verify(content)) {
+      fs.unlinkSync(tmpFile)
+      return content
+    } else {
+      return prompt('You entered a not valid document. Do you want to continue (c), reset (r) or abort (a): ')
+      .then((res) => {
+        process.stdin.pause()
+        if (res === 'c') {
+          return editContent(content, fileType, verify)
+        } else if (res === 'r') {
+          return editContent(initialContent, fileType, verify)
+        } else {
+          throw new Error('User aborted editing.')
+        }
+      })
+    }
+  })
+}
+
+/**
+ * Gets the input for a CLI process by
+ *
+ *  1. Checking if the input file is given and okay.
+ *  2. If no input file is given: Look for input on stdin
+ *  3. Else start an editor to get user input. It will spawn the editor specified by
+ *     the `EDITOR` environment variable or if none is set it will try to spawn nano.
+ *
+ * @params {string} file The path to a file.
+ * @params [inStream] An input stream. If none is given `process.stdin` will be used.
+ * @returns {Promise<string>} Returns a promise that resolves the input string
+ */
+function fileStdinOrEdit (file, {inStream = process.stdin, fileType = '.json', defaultContent = '', verify = null}) {
+  if (file && file.length > 0) {
+    return getFileContents(file)
+  } else if (!process.stdin.isTTY) {
+    return getStdin()
+  } else {
+    editContent(defaultContent, fileType, verify)
+  }
+}
+
+module.exports = {
+  input: fileStdinOrEdit,
+  edit,
+  editContent
+}
---
+++ test/test.js
@@ -0,0 +1,6 @@
+
+import test from 'ava'
+// import * as cliExt from '../index.js'
+
+test((t) => {
+})
